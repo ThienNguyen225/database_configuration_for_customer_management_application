e5fbed5a2b2b973b5b87c05c2b2c8e9a9b812130
Your Name <you@example.com>
2019-07-01 11:08:46 +0700

    Cấu hình CSDL cho ứng dụng quản lý khách hàng

---
+++ database/seeds/CustomerTableSeeder.php
@@ -0,0 +1,32 @@
+<?php
+
+use App\Customer;
+use Illuminate\Database\Seeder;
+
+class CustomerTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+//     $customer = new Customer();
+//     $customer->name = 'Nguyễn Thiên';
+//     $customer->dob = '2222-02-2';
+//     $customer->email = 'nguyenthien@gmail.com';
+//     $customer->save();
+        $dataArray = [];
+        for ($i = 0; $i < 20; $i++) {
+            array_push($dataArray, [
+                'name' => str_random(10),
+                'dob' => date("Y-m-d", mt_rand(1, time())),
+                'email' => str_random(10) . '@gmail.com',
+                'created_at' => date('Y-m-d H:i:s'),
+                'updated_at' => date('Y-m-d H:i:s')
+            ]);
+        }
+        DB::table('customers')->insert($dataArray);
+    }
+}
